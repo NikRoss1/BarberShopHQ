9853e9c84226516b698732b2d5fc312a5c5571d2
Nikolay Panov <nik.cmkw@yandex.ru>
2022-08-20 18:41:10 +0500

    Add Clients migration

--- Gemfile
+++ Gemfile
@@ -10,3 +10,5 @@ gem "sinatra-contrib"
 group :development do
 	gem "tux"
 end
+
+
--- app.rb
+++ app.rb
@@ -1,14 +1,20 @@
 #encoding: utf-8
 require 'rubygems'
 require 'sinatra'
-require "sinatra/activerecord"
-require "sinatra/activerecord/rake"
-
+require 'sinatra/activerecord'
 
 set :database, {adapter: "sqlite3", database: "barbershop.db"}
 
 class Client < ActiveRecord::Base
 end
+
+class Barber < ActiveRecord::Base 
+
+end
+
+
+
+
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
